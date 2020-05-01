2ce1a0fcf1a07952f4cf6d3c0d394af2dcfb4b92
onuriltan <onur.iltan@gmail.com>
2020-05-01 17:23:00 +0200

    add eslint and prettier configurations

---
+++ packages/server/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended"
+  ],
+  rules: {
+    "indent": ["error", 2],
+    "object-curly-spacing": [2, "always"],
+  }
+};
