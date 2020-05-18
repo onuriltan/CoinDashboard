f32e8c085d334a3f9fcea57d7d089789292c181c
onuriltan <onur.iltan@gmail.com>
2020-05-18 09:17:56 +0200

    actually mock the child component inside App component

--- packages/client/src/app/App.test.tsx
+++ packages/client/src/app/App.test.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import { MockedProvider } from '@apollo/react-testing'
-import * as am4core from '@amcharts/amcharts4/core'
 import { App } from './App'
-import { mockChartObject } from '../config/test/GlobalMocks'
 
 jest.mock('@amcharts/amcharts4/core')
 jest.mock('@amcharts/amcharts4/charts')
+jest.mock('../modules/bitcoin-chart/BitcoinChart', () => {
+  return {
+    __esModule: true,
+    A: true,
+    default: () => {
+      return <div/>
+    }
+  }
+})
 const wrappedComponent = () => {
   return (
     <MockedProvider addTypename={false}>
@@ -18,7 +25,6 @@ const wrappedComponent = () => {
 describe('<App />', function () {
   afterEach(cleanup)
   it('should render without crashing', async () => {
-    jest.spyOn(am4core, 'create').mockImplementation(() => mockChartObject)
     render(wrappedComponent())
   })
 })
